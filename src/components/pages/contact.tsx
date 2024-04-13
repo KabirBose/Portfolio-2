d4da400acce36c5438becb07d8105f78ba57cc4a
Kabir Bose <kabirbose04@gmail.com>
2024-04-13 11:19:48 -0400

    added fontawesome icons

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -1,5 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 export default function Contact() {
   return (
@@ -7,7 +9,24 @@ export default function Contact() {
       <h1 className="text-green-500 font-space text-2xl font-bold">CONTACT</h1>
       <div>
         <p>You can check out my projects here or contact me if you'd like!</p>
-        <FontAwesomeIcon icon={faEnvelope} />
+
+        <div className="mt-5 flex justify-center items-center gap-12">
+          <FontAwesomeIcon
+            color="#22C55E"
+            className="w-[2rem] h-[2rem]"
+            icon={faEnvelope}
+          />
+          <FontAwesomeIcon
+            color="#22C55E"
+            className="w-[2rem] h-[2rem]"
+            icon={faGithub}
+          />
+          <FontAwesomeIcon
+            color="#22C55E"
+            className="w-[2rem] h-[2rem]"
+            icon={faLinkedin}
+          />
+        </div>
       </div>
     </div>
   );
