2e4de5fc0f43ae6219ee1ea47c0c44f5ca8ca281
Kabir Bose <kabirbose04@gmail.com>
2024-04-12 22:52:00 -0400

    added font-awesome package

--- src/components/pages/contact.tsx
+++ src/components/pages/contact.tsx
@@ -1,7 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
 export default function Contact() {
   return (
     <div className="min-h-[80vh] p-4 flex justify-center items-center text-center flex-col gap-2">
       <h1 className="text-green-500 font-space text-2xl font-bold">CONTACT</h1>
+      <div>
+        <p>You can check out my projects here or contact me if you'd like!</p>
+        <FontAwesomeIcon icon={faEnvelope} />
+      </div>
     </div>
   );
 }
